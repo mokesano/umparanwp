c26141543606860ed80d9d5655a7513bfe254397
Rochmady <kampo.mokesano@gmail.com>
2026-04-25 21:05:16 +0700

    Initialize and update content files requires

--- wp-content/themes/umparan/index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-<div class="content">
-<?php get_template_part("template-parts/header/index"); ?>
-<?php get_template_part("template-parts/menu/index"); ?>
-<?php get_template_part("template-parts/home/index"); ?>
-</div>
-<?php get_footer(); ?>
